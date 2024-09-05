517a5a1a0aa754a2bb594dee308c29dd6a670aec
Jeff Scholen <jscholen@ioufinancial.com>
2024-09-05 15:56:13 -0400

    fix build

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /app/tmp/pids/server.pid
+rm -f /var/run/puma.pid
+
+# Configure DATABASE_URL if MYSQL_URL is set
+if [ -n "${POSTGRES_URL}" ]; then
+  echo "Setting DB_ADAPTER=postgresql"
+  export DB_ADAPTER=postgresql
+  echo "Configuring DATABASE_URL using POSTGRES_URL"
+  export DATABASE_URL="${POSTGRES_URL}"
+fi
+
+echo "Running migrations..."
+bundle exec rails db:migrate
+echo "Finished running migrations"
+
+echo "Precompile Assets on entrypoint"
+SECRET_KEY_BASE=1 bundle exec rake assets:precompile
+echo "Finished precompiling assets"
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
